46fd1bd6d65adfd2119ffc6b6645f87d5dab7dc7
Ahmed Rebai <ahmed.rebai2@gmail.com>
2024-06-09 18:06:11 +0200

    Update Migrate_Snowflake_to_BigQuery.sql

--- Migrate_Snowflake_to_BigQuery.sql
+++ Migrate_Snowflake_to_BigQuery.sql
@@ -31,6 +31,47 @@ from files (
 
 -- Task 2. Update BigQuery table data using DML statements
 
+INSERT INTO 
+  animals_dataset.owners (OwnerID, Name) 
+VALUES 
+  (9, 'Mary'); 
+
+INSERT INTO 
+  animals_dataset.pets (PetID, OwnerID, Type, Name, Weight) 
+VALUES 
+    (28, 9, 'Dog', "George", 50); 
+
+
+INSERT INTO 
+  animals_dataset.pets (PetID, OwnerID, Type, Name, Weight) 
+VALUES 
+  (29, 9, 'Dog', "Washington", 60);
+
+SELECT o.Name, p.Type, p.Name 
+FROM 
+  animals_dataset.owners o 
+JOIN 
+  animals_dataset.pets p 
+ON 
+  o.OwnerID = p.OwnerID
+WHERE 
+  o.Name = 'Mary'; 
+
+UPDATE 
+  animals_dataset.pets
+SET 
+  Type = 'Canine'
+WHERE 
+  Type = 'Dog'; 
+
+DELETE FROM
+  animals_dataset.pets
+WHERE 
+  Type = 'Frog';
+
+-- Task 3. Join data and write CTEs using SQL SELECT statements
+
+
 
 
 
