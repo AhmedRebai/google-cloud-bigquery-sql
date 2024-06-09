73c0428f24c2fa9a19ddf8c795080bea8a938dac
Ahmed Rebai <ahmed.rebai2@gmail.com>
2024-06-09 17:46:55 +0200

    Update Migrate_Snowflake_to_BigQuery.sql

--- Migrate_Snowflake_to_BigQuery.sql
+++ Migrate_Snowflake_to_BigQuery.sql
@@ -1,1 +1,34 @@
+-- Task 1. Create BigQuery dataset and tables using DDL statements
+
+
+-- create schema if not exists animals_dataset options(location="us");
+
+-- create table animals_dataset.owners (OwnerID int64 not null, name string);
+
+-- create table animals_dataset.pets(
+--   PetID int64 not null,
+--   OwnerID int64 not null, 
+--   Type string,
+--   Name string,
+--   Weight float64
+-- );
+
+-- load data into animals_dataset.owners 
+-- from files (
+--   skip_leading_rows = 1,
+--   format = 'CSV',
+--   field_delimiter = ',',
+--   uris = ['gs://tcd_repo/data/environmental/animals/owners.csv']
+-- );
+
+load data into animals_dataset.pets 
+from files (
+  skip_leading_rows = 1,
+  format = 'CSV',
+  field_delimiter = ",",
+  uris = ['gs://tcd_repo/data/environmental/animals/pets.csv']
+);
+
+
+
 
